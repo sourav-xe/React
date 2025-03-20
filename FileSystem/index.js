4f182fbe173cc7696654880f1e080008e8b8ae36
sourav-xe <sourav20kumar02@gmail.com>
2025-03-20 15:59:10 +0530

    Exploring React

---
+++ FileSystem/index.js
@@ -0,0 +1,7 @@
+// import fs from "fs";
+
+// // fs.writeFileSync("./ok.txt", "thisi is a new file");
+// // fs.appendFileSync("./ok.txt", "\nchnesis adsale");
+
+// fs.unlinkSync("./weekdays.txt")
+
